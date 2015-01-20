d603c8a0230eddbb10d65a76a240cef61eb19730
Derek Brown <derek@derek.am>
2015-01-19 20:21:41 -0800

    Remove SecureSocial Plugins, Move Provider Config to GlobalSettings

--- app/Global.scala
+++ app/Global.scala
@@ -3,13 +3,19 @@ package common
 import models._
 import play.api._
 import securesocial.core.RuntimeEnvironment
+import securesocial.core.providers._
+import securesocial.core.providers.utils.{Mailer, PasswordHasher, PasswordValidator}
 import services.KnotisUserService
 import java.lang.reflect.Constructor
+import scala.collection.immutable.ListMap
 
 object Global extends play.api.GlobalSettings {
 
   object KnotisRuntimeEnvironment extends RuntimeEnvironment.Default[User] {
     override lazy val userService: KnotisUserService = new KnotisUserService(play.api.Play.current)
+    override lazy val providers = ListMap(
+      include(new UsernamePasswordProvider[User](userService, avatarService, viewTemplates, passwordHashers))
+    )
   }
 
   override def getControllerInstance[ControllerType](controllerClass: Class[ControllerType]): ControllerType = {
