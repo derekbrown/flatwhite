731d682ebf06f4ddb56780f74d5faa37965f8ef2
Derek Brown <derek@derek.am>
2015-01-24 21:08:10 -0800

    Fixed Instantiation Issues!

--- app/Global.scala
+++ app/Global.scala
@@ -1,7 +1,5 @@
-package common
-
 import models._
-import play.api._
+import play.api.GlobalSettings
 import securesocial.core.RuntimeEnvironment
 import securesocial.core.providers._
 import securesocial.core.providers.utils.{Mailer, PasswordHasher, PasswordValidator}
@@ -18,12 +16,12 @@ object Global extends play.api.GlobalSettings {
     )
   }
 
-  override def getControllerInstance[ControllerType](controllerClass: Class[ControllerType]): ControllerType = {
+  override def getControllerInstance[A](controllerClass: Class[A]): A = {
     val instance = controllerClass.getConstructors.find { c =>
       val params = c.getParameterTypes
       params.length == 1 && params(0) == classOf[RuntimeEnvironment[User]]
     }.map {
-      _.asInstanceOf[Constructor[ControllerType]].newInstance(KnotisRuntimeEnvironment)
+      _.asInstanceOf[Constructor[A]].newInstance(KnotisRuntimeEnvironment)
     }
     instance.getOrElse(super.getControllerInstance(controllerClass))
   }
